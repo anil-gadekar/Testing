f3ba37c721f8c742a1443b41c881ebed6301b6e7
Anil Gadekar <anil-gadekar@vwdomatrix.net>
2018-12-13 16:05:21 +0530

    from lab project updated

---
+++ src/main/java/com/vedomatrix/vyom_lab/VyomLabApplication.java
@@ -0,0 +1,13 @@
+package com.vedomatrix.vyom_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VyomLabApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(VyomLabApplication.class, args);
+    }
+}
